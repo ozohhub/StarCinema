ced2816cf20b7898c666ac54ecced126b3164a52
yuna1313 <93983333+yuna1313@users.noreply.github.com>
2021-11-09 20:35:08 +0900

    test

--- src/main/java/com/star/cinema/HomeController.java
+++ src/main/java/com/star/cinema/HomeController.java
@@ -30,7 +30,7 @@ public class HomeController {
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
 		String formattedDate = dateFormat.format(date);
-		
+		System.out.println("ㅇㅇ");
 		model.addAttribute("serverTime", formattedDate );
 		
 		return "home";
